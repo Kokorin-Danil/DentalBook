cff1e4018c2351c4cbdb67ec0574c15851c7568e
Kokorin-Danil <kokorindanil474@gmail.com>
2025-02-12 01:24:16 +0400

    Исправил валидацию

--- backend/doctors/routerDoctor.js
+++ backend/doctors/routerDoctor.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import controllerDoctor from "./controllerDoctor.js"; // Импортируем контроллер для создания врача
 
 const doctorRouter = Router();
 
 // Роут для создания врача с пользователем
-doctorRouter.post("/create", controllerDoctor.createDoctor);
+doctorRouter.post(
+  "/create",
+  [
+    body("mobilePhone")
+      .matches(/^\+7\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$/)
+      .withMessage("Номер телефона должен быть в формате +7 (999) 123-45-67"),
+  ],
+  controllerDoctor.createDoctor
+);
 doctorRouter.get("/profile", controllerDoctor.getDoctorInfo);
 doctorRouter.get("/shudle", controllerDoctor.getDoctorVisits);
 doctorRouter.get("/get/all", controllerDoctor.getAllDoctors);
